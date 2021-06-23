b2c7dab75f3c0a908f60cfc37b7de4e987b6b181
Dheeraj R <83569971+DheerajR1@users.noreply.github.com>
2021-06-23 23:35:54 +0530

    WIP : clustering

--- JS/app.js
+++ JS/app.js
@@ -189,14 +189,22 @@ function sortCord() {
 
             // Clustering code...
             var maxDist = 1.00;
-            var clusterList = [];
+            var clusterList = [[],[]];
+            var clusterIndex = 0;
+            var counter = 0; //nodes in a cluster
             for (var k = 0; k < (visitedRouteList.length - 1); k++) {
                 if ((distance[visitedRouteList[k]][visitedRouteList[k + 1]]) < maxDist) {
+                    console.log("TEST:",clusterList[clusterIndex]);
+                    clusterList[clusterIndex][count] = (completecoords[visitedRouteList[k]]);
+                    count++;
                     console.log(completecoords[visitedRouteList[k]] + " : " + completecoords[visitedRouteList[k + 1]] + ":   :  " + distance[visitedRouteList[k]][visitedRouteList[k + 1]]);
                 } else {
+                    clusterIndex++;
+                    count = 0;
                     console.log("");
                 }
             }
+            console.log({clusterList});
         }
     }
 }
